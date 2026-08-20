3a88e3a6e10eb82623a33caf3d29b411455be9b7
ahmed <ahmedp4f@gmail.com>
2026-08-20 15:24:09 +0100

    added JWT auth system

---
+++ backend/dependencies/dependencies.py
@@ -0,0 +1,40 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database.database import get_db
+from models.models import User
+from schemas.user import UserResponse
+from services.security import verify_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    current_user_id = verify_access_token(token)
+
+    if current_user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
+
+    user = db.query(User).filter(
+        User.id == int(current_user_id)
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+
+    return UserResponse(
+        username=user.username,
+        email=user.email
+    )
